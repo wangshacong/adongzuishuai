e4537d2e14d00c5d25d5aa4f30c17672a67f7fa4
wangshacong <pnndt@qq.com>
2019-01-04 09:59:08 +0800

    更新

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -81,25 +81,29 @@
                             <a title="删除" href="news1/{{$v['id']}}/destroy" onclick="member_del(this,'1')" style="text-decoration:none">
                                 <i class="layui-icon">&#xe640;</i>
                             </a>
-                            <script language="JavaScript">             
-                                    function delete_confirm(e) 
-                                    {
-                                        if (event.srcElement.outerText == "删除") 
-                                        {
-                                            event.returnValue = confirm("删除是不可恢复的，你确认要删除吗？");
-                                        }
+                            <script language="JavaScript">
+                                function delete_confirm(e) {
+                                    if (event.srcElement.outerText == "删除") {
+                                        event.returnValue = confirm("删除是不可恢复的，你确认要删除吗？");
                                     }
-                                    document.onclick = delete_confirm;
-                                    </script>
-                                    <a href="news1/{{$v['id']}}/destroy" onClick="delete_confirm">删除</a>
+                                }
+                                document.onclick = delete_confirm;
+                            </script>
+                            <a title="编辑" href="#">
+                                <i class="layui-icon">&#xe642;</i>
+                            </a>
+                            <a title="删除" href="news1/{{$v['id']}}/destroy" onClick="delete_confirm">
+                                删除
+                                {{-- <i class="layui-icon" onClick="delete_confirm">&#xe640;</i> --}}
+                            </a>
 
                         </td>
                     </tr>
                     @endforeach
-                    
-                   
+
+
                 </tbody>
-                
+
             </table>
             {{$user->appends(request()->all())->links()}}
             <!-- 右侧内容框架，更改从这里结束 -->
